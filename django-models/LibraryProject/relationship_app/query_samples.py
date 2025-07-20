d68213e083cb8bba719876792edbeef2b681eb56
oluwatobi <ojothrby@gmail.com>
2025-07-20 12:11:44 +0200

    Fix: Add required Library.objects.get line to query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,13 +7,12 @@ django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Now you can query like this:
-author_name = "Chinua Achebe"
-author = Author.objects.get(name=author_name)
-books_by_author = author.books.all()
+# ... (author query here)
 
-print(f"Books by {author_name}:")
-for book in books_by_author:
+library_name = "Central City Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+
+print(f"\nBooks in {library_name}:")
+for book in books_in_library:
     print(f"- {book.title}")
-
-# ... add other queries
