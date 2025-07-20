b06c1b2d41d77b70624ebf007be96cfcf19a90fe
oluwatobi <ojothrby@gmail.com>
2025-07-20 12:24:17 +0200

    Fix: Add Author.objects.get and Book.objects.filter to query_samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,22 @@
 import os
 import django
 
-# Set the correct settings module (case sensitive!)
+# Set the correct settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# ... (author query here)
+# 1. Query all books by a specific author using filter
+author_name = "Chinua Achebe"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
 
+print(f"Books by {author_name}:")
+for book in books_by_author:
+    print(f"- {book.title}")
+
+# 2. List all books in a library
 library_name = "Central City Library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
@@ -16,3 +24,7 @@ books_in_library = library.books.all()
 print(f"\nBooks in {library_name}:")
 for book in books_in_library:
     print(f"- {book.title}")
+
+# 3. Retrieve the librarian for a library
+librarian = library.librarian
+print(f"\nLibrarian of {library_name}: {librarian.name}")
