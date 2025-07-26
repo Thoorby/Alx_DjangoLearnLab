113273e0b46ef52479b24204ac99acc51a0e88d4
oluwatobi <ojothrby@gmail.com>
2025-07-26 10:11:22 +0200

    Reorganized project structure; updated relationship_app with views, urls, templates, and migrations

--- django-models/django-models/Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns to show
-    search_fields = ('title', 'author')                     # Search bar
-    list_filter = ('publication_year',)                     # Filter sidebar
