6070a42b8edef1ad4e32e87b5fbdc35668f03701
oluwatobi <ojothrby@gmail.com>
2025-07-27 14:56:07 +0200

    Use DetailView from django.views.generic.detail to satisfy checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import DetailView 
+from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
 
