4fd0961b41ff0ff635d59aa1aaa251967652e666
oluwatobi <ojothrby@gmail.com>
2025-07-28 22:38:25 +0200

    Fix URL patterns to match required 'add_book/' and 'edit_book/' formats

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,11 +6,11 @@ from .librarian_view import librarian_view
 from .member_view import member_view
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),  # changed
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),  # changed
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 
+    path('books/', views.list_books, name='list_books'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
