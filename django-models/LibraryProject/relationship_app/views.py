4076645bca69324d444bec26bfbc4959a1c22c52
oluwatobi <ojothrby@gmail.com>
2025-07-28 22:19:12 +0200

    Secure book views with @permission_required decorators

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.contrib.auth import login
