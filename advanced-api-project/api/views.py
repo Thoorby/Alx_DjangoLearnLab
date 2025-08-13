cf1c4ac95c9793c7013e646be2ed104376e06de9
oluwatobi <ojothrby@gmail.com>
2025-08-13 18:04:16 +0200

    feat(api): add filtering, searching, ordering, and role-based permissions to book API views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,8 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, filters
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
 from django.shortcuts import get_object_or_404
 from .models import Book, Author
 from .serializers import BookSerializer, BookCreateUpdateSerializer, AuthorSerializer
