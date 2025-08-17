6551302c3e92406a1c4fe2e9fd587e1eec186b98
oluwatobi <ojothrby@gmail.com>
2025-08-17 14:03:36 +0200

    chore: add search and tag-based post URLs with PostByTagListView

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
+from .views import PostByTagListView, search_posts
 from .views import PostByTagListView
-from .views import search_posts, posts_by_tag
 from .views import (
     register_view, login_view, logout_view, profile_view,
     PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
