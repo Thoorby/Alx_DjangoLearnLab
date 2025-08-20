d91e71262aec3123d9a6285dd092c396e037810c
oluwatobi <ojothrby@gmail.com>
2025-08-20 13:18:02 +0200

    fixing the urls

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
-    path("api/", include("posts.urls")), 
+]
+urlpatterns= [
+    path("api/", include("posts.urls")),
 ]
