0de8e20ed32ff86b23dc9cb5043e672120d7f6f7
oluwatobi <ojothrby@gmail.com>
2025-08-20 13:48:35 +0200

    making adjustment to the post/views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,31 +1,31 @@
 from rest_framework import viewsets, permissions
-from rest_framework.exceptions import PermissionDenied
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
+
+
+# Custom permission to allow only owners to edit/delete
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # SAFE_METHODS are GET, HEAD, OPTIONS → allow anyone
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Otherwise, only the owner/author can modify
+        return obj.author == request.user
+
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related('author').all()
+    queryset = Post.objects.all()  
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    search_fields = ['title', 'content']        
-    ordering_fields = ['created_at', 'updated_at', 'title']  
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()  
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    search_fields = ['content']                 
-    ordering_fields = ['created_at', 'updated_at']
-
-    def get_queryset(self):
-        qs = Comment.objects.select_related('author', 'post').all()
-        post_id = self.request.query_params.get('post')
-        if post_id:
-            qs = qs.filter(post_id=post_id)
-        return qs
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
